9fe24a8c8cd2d12947cdabf964272920583cd4ce
Thuc Truong <congthuc26@gmail.com>
2020-05-19 10:22:25 +0700

    update babel-plugin-root-import devDependencies

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,28 @@ module.exports = function(api) {
   api.cache(true);
   return {
     plugins: [
-      ["@babel/plugin-proposal-decorators", { "legacy": true }]
+      ["@babel/plugin-proposal-decorators", { "legacy": true }],
+      [
+        "babel-plugin-root-import", 
+        {
+          "rootPathPrefix": "~",
+          "rootPathSuffix": "src"   
+        }
+      ]
     ],
     presets: ['babel-preset-expo'],
+    "env": {
+      "production": {
+        "plugins": [
+          [
+            "babel-plugin-root-import",
+            {
+              "rootPathPrefix": "~",
+              "rootPathSuffix": "src"
+            }
+          ]
+        ]
+      }
+    }
   };
 };
