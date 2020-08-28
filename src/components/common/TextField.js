e1ee2ddbcac65ead459203cf61f6288a3db22697
DatCodero <chipcop106@gmail.com>
2020-08-28 17:27:21 +0700

    Update text input use local change

--- src/components/common/TextField.js
+++ src/components/common/TextField.js
@@ -1,44 +1,21 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Input, Icon } from "@ui-kitten/components";
-import { color } from "~/config";
+import React, { useEffect, useState } from "react";
+import { Input } from "@ui-kitten/components";
 const TextField = (props) => {
-    return (
-        <View>
-            <Input
-                {...props}
-                disabled={props.disabled}
-                style={props.disabled ? styles.disabledInput : props.input}
-                textStyle={props.disabled ? styles.textStyle : props.textStyle}
-                accessoryRight={() =>
-                    props.disabled && (
-                        <>
-                            <Icon
-                                name="lock"
-                                fill={color.primary}
-                                style={{
-                                    width: 25,
-                                    height: 25,
-                                }}
-                            />
-                        </>
-                    )
-                }
-            />
-        </View>
-    );
+  const [value, setValue] = useState(props?.value ?? "");
+  const _onChangeText = (text) => {
+    setValue(text);
+  };
+  const _onEndEditing = (text) => {
+    props.textChange(text);
+  };
+  return (
+    <Input
+      {...props}
+      value={value}
+      onChangeText={_onChangeText}
+      onEndEditing={_onEndEditing}
+    />
+  );
 };
 
 export default TextField;
-
-const styles = StyleSheet.create({
-    input: {
-        width: "100%",
-    },
-    disabledInput: {
-        width: "100%",
-    },
-    textStyle: {
-        color: color.darkColor,
-    },
-});
